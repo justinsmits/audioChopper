ebbc5cd76724c6ce44913b1b71bd830260ca4a96
Justin Smits <justin.smits@relativity.com>
2020-03-01 15:50:39 -0600

    adding option parsing logic

--- audioChopper/Program.cs
+++ audioChopper/Program.cs
@@ -4,6 +4,7 @@ using CSCore.Codecs;
 using CSCore.MediaFoundation;
 using CommandLine;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SplitAudio
 {
@@ -12,10 +13,10 @@ namespace SplitAudio
 
         class Options
         {
-            [Option('r', "read", Required = true, HelpText = "Input files to be processed.")]
-            public IEnumerable<string> InputFiles { get; set; }
+            [Option('r', "read", Required = true, HelpText = "Input file to be processed.")]
+            public String InputFile { get; set; }
 
-            [Option('t', "time", Required = true, HelpText = "Comma delimted input string of times to split out. Format follows <starttime>|<time span>. Ex: 01:24|03:12,01:22:14|4:56")]
+            [Option('t', "time", Required = true, HelpText = "Comma delimted input string of times to split out. Format follows <starttime>|<time span>. Ex: 01:24|03:12,01:22:14|4:56", Separator = ',')]
             public IEnumerable<String> InputTimes { get; set; }
 
             // Omitting long name, defaults to name of property, ie "--verbose"
@@ -33,6 +34,16 @@ namespace SplitAudio
             public long? Offset { get; set; }
         }
 
+        class ChopJob
+        {
+            public TimeSpan StartTime { get; set; }
+            public TimeSpan Span { get; set; }
+            public String OutFileName { get; set; }
+        }
+
+        //ewwww, but perfect is the enemy of good
+        private static IEnumerable<ChopJob> Jobs = new System.Collections.Generic.List<ChopJob>();
+        private static String InputFilePath;
 
         static void Main(string[] args)
         {
@@ -42,21 +53,74 @@ namespace SplitAudio
     .WithParsed(RunOptions)
     .WithNotParsed(HandleParseError);
 
+
+
             return;
             TimeSpan startTimeSpan = TimeSpan.FromSeconds(4920);
             TimeSpan endTimeSpan = TimeSpan.FromSeconds(5160);
 
-            using (IWaveSource source = CodecFactory.Instance.GetCodec(@"C:\Source2\282.mp3"))
-            using (MediaFoundationEncoder mediaFoundationEncoder =
-                MediaFoundationEncoder.CreateWMAEncoder(source.WaveFormat, @"C:\Source2\282cut.mp3"))
+            using (IWaveSource source = CodecFactory.Instance.GetCodec(InputFilePath))
             {
-                AddTimeSpan(source, mediaFoundationEncoder, startTimeSpan, endTimeSpan);
+                using (MediaFoundationEncoder mediaFoundationEncoder =
+                                MediaFoundationEncoder.CreateWMAEncoder(source.WaveFormat, @"C:\Source2\282cut.mp3"))
+                {
+                    AddTimeSpan(source, mediaFoundationEncoder, startTimeSpan, endTimeSpan);
+                }
+
             }
+
         }
+
+        private static TimeSpan GetTimespan(String input)
+        {
+            TimeSpan retVal;
+            if (!(input.Count(c => c == ':') > 1))
+            {
+                input = "00:" + input;
+            }
+            if (!System.TimeSpan.TryParse(input, out retVal))
+            {
+                Console.WriteLine("ERROR, not able to parse the following value into a timespan: " + input);
+                throw new ArgumentOutOfRangeException("Invalid timespan: " + input);
+            }
+            return retVal;
+        }
+
+        private static Tuple<TimeSpan, TimeSpan> GetJobTimes(String input)
+        {
+            TimeSpan startTime;
+            TimeSpan span;
+            if (!(input.IndexOf('|') > 0))
+            {
+                throw new ArgumentException("Incorrect time entered: " + input);
+            }
+
+            String[] split = input.Split('|');
+            startTime = GetTimespan(split[0]);
+            span = GetTimespan(split[1]);
+
+            return new Tuple<TimeSpan, TimeSpan>(startTime, span);
+        }
+
 
         static void RunOptions(Options opts)
         {
             //handle options
+            InputFilePath = opts.InputFile;
+            Int32 i = 0;
+            String inputDirectory = System.IO.Path.GetDirectoryName(opts.InputFile);
+            String inputFileName = System.IO.Path.GetFileNameWithoutExtension(opts.InputFile);
+            String inputFileExtension = System.IO.Path.GetExtension(opts.InputFile);
+            foreach(var timez in opts.InputTimes)
+            {
+                ChopJob cj = new ChopJob();
+                Tuple<TimeSpan, TimeSpan> jobTimes = GetJobTimes(timez);
+                cj.StartTime = jobTimes.Item1;
+                cj.Span = jobTimes.Item2;
+                String finalFilePath = System.IO.Path.Combine(inputDirectory, (inputFileName + i.ToString()), inputFileExtension);
+                cj.OutFileName = finalFilePath;
+            }
+
         }
         static void HandleParseError(IEnumerable<Error> errs)
         {
